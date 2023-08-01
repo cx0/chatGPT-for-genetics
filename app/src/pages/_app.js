1ff43afc8977f2ad19944f48a7eb3c2ef13da2e4
Foad Green <foadgreen@gmail.com>
2023-08-01 11:16:12 -0700

    Adding GA4 tag

--- app/src/pages/_app.js
+++ app/src/pages/_app.js
@@ -1,12 +1,16 @@
+// _app.js
+import React from 'react';
 import '../styles/global.css';
 import ReactGA from 'react-ga';
 
+ReactGA.initialize('G-GRYK5L4WC3');
+
 function MyApp({ Component, pageProps }) {
     React.useEffect(() => {
-        ReactGA.initialize('G-GRYK5L4WC3');
-        ReactGA.pageview(window.location.pathname + window.location.search);
+        if (process.env.NODE_ENV === 'production') {
+            ReactGA.pageview(window.location.pathname + window.location.search);
+        }
     }, []);
-
     return <Component {...pageProps} />;
 }
 
